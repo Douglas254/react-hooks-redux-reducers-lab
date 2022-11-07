623e7cbb529c70a56eecccad5447c86263ab5df8
Douglas <douglasobara97@gmail.com>
2022-11-07 06:20:40 +0300

    commit tree 90e06b3305b53922defbc833e3f26c9420f127b8

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,16 @@
+    // ✓ returns the existing state if the action's type doesn't match a type in the reducer 
+    // ✓ increases the number of presents if there the action's type is 'presents/increase' 
+    // ✓ adheres to the rules of being a pure function, by not changing the original state, and instead returning a new object 
+
 const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
   // your code here
+  switch (action.type) {
+    case "presents/increase":
+      return { numberOfPresents: initialState.numberOfPresents + 1 };
+
+    default:
+      return state;
+  }
 }
