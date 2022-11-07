c47641e16a640f15fb7e07ed271d6f5f7fb1ce28
Douglas <douglasobara97@gmail.com>
2022-11-07 06:22:44 +0300

    Add my solution

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,22 @@
+// adds the friend when type is 'friends/add' and the action has a payload property with a name, hometown and id
+// removes the friend when action type is 'friends/remove' and the action has a property of the friends id to be removed
+// adheres to the rules of being a pure function, by not changing the original state, and instead returning a new object
+// returns the existing state if the action's type doesn't match a type in the reducer
+
 export function manageFriends(state, action) {
   // your code here
+  switch (action.type) {
+    case "friends/add":
+      return { friends: [...state.friends, action.payload] };
+
+    case "friends/remove":
+      return {
+        friends: [
+          ...state.friends.filter((friend) => friend.id !== action.payload),
+        ],
+      };
+
+    default:
+      return state;
+  }
 }
